2b61b28021255204912dda3a93627cdecd25ebd6
Kacper Wołoszyn <budzio161@gmail.com>
2018-02-15 10:32:50 +0100

    Add all needed alements

---
+++ index.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+	response.setHeader('Content-Type', 'text/html', 'utf-8');
+	if (request.method === 'GET' && request.url === '/webOne') {
+		fs.readFile('./index.html', function (err, data) {
+			if (err) throw err;
+			response.write(data);
+			response.end();
+			
+		});
+	} else {
+		response.statusCode = 404;
+		fs.readFile('./four-oh-four_08.jpg', function (err, data) {
+			if (err) throw err;
+			response.setHeader('Content-Type', 'image/jpg', 'utf-8');
+			response.write(data);
+			response.end();
+		});
+	}
+});
+
+server.listen(9000);
